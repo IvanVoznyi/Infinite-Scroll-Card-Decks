31e274da3fecacaaa7587f8ce3dbd5cbecb5746c
Ivan Voznyi <ivan.voznyi92@gmail.com>
2024-02-13 20:16:18 +0200

    Initial Commit

---
+++ CardDeck/CardDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardDeckApp.swift
+//  CardDeck
+//
+//  Created by Ivan Voznyi on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
